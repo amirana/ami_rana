a3bc58ea431563f4ad0a456edd988986a5d8183f
Ami Rana <ranaami1223@gmail.com>
2023-06-28 01:53:17 +0530

    Resume

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -5,60 +5,68 @@
             <h2 class="text-4xl font-bold py-20 text-center">Some of My Works</h2>
             <ul class="flex flex-wrap">
                 <li class="w-1/2 p-16 text-center border-b group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-solid border-l border-b group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-b group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-solid border-l border-b group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-b group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-solid border-l border-b group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-b-0 group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
                 <li class="w-1/2 p-16 text-center border-solid border-l group relative overflow-hidden">
-                    <a href="#" class="text-xl">Ami Rana Portfolio</a>
-                    <div class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
-                        <h3>Portfolio website</h3>
-                        <p>Built with HTML, Tailwind CSS and VueJS</p>
-                    </div>
+                    <a href="https://ami-rana.vercel.app/" class="text-xl" target="_blank">Ami Rana Portfolio
+                       <span class="hover__content text-base w-full h-full bg-purple text-white flex flex-col items-center justify-center invisible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity transition-bg duration-300 group-hover:visible group-hover:opacity-100">
+                            <span class="font-semibold mb-3">Portfolio website</span>
+                            <span class="text-sm">Built with HTML, Tailwind CSS and VueJs</span>
+                       </span> 
+                    </a>
                 </li>
             </ul>
         </div>
